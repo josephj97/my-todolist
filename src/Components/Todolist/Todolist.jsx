5a277f1ca0bacf7489c23c789c20f20023425a22
Joseph Atef <josephatef.j97@gmail.com>
2024-02-11 04:20:01 +0200

    Make Completed to be a checkbox (still not working)

--- src/Components/Todolist/Todolist.jsx
+++ src/Components/Todolist/Todolist.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import './Todolist.css'
+import Checkbox from '@mui/material/Checkbox';
+
 
 const Todolist = () => {
     const [todolist, setTodolist] = useState();
@@ -14,15 +16,14 @@ const Todolist = () => {
     });
 
     return (
-        <div className='container paddings innerWidth flexCenter'>
+        <div className='container paddings innerWidth flexColStart'>
             {todolist && todolist.map(todo => {
                 const { id, title, completed } = todo
                 return (
-                    <div className='card'
-                        onClick={() => navigate(`/todos/${id}`)}
-                    >
-                        <h4>{title}</h4>
-                        <h6>{`Completed: ${completed}`}</h6>
+                    <div className='card innerWidth'>
+                        <h4 className='title flexEnd' onClick={() => navigate(`/todos/${id}`)}>{title}</h4>
+                        {/* <h6>{`Completed: ${completed}`}</h6> */}
+                        <Checkbox className='flexEnd' size='small' />
                     </div>
                 )
             })}
