8ec09ae93aded36981d981a82fbfa73eb3be9663
Joseph Atef <josephatef.j97@gmail.com>
2024-02-18 01:10:37 +0200

    We can add tasks using axios

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Todolist from '../Todolist/Todolist';
 import TodoItem from '../TodoItem/TodoItem';
 import Header from '../Header/Header'
+import TodoForm from '../TodoForm/TodoForm';
 
 function Dashboard() {
     return (
         <div>
             <Header />
+            <TodoForm />
             <Router>
                 <Routes>
                     <Route exact path="/" element={<Todolist />} />
