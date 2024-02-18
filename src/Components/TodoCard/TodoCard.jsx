aa45ab73e19ab2e48ac904cd15bb97f45d6bc52c
Joseph Atef <josephatef.j97@gmail.com>
2024-02-19 01:22:09 +0200

    Checkbox change can be updated into the database

--- src/Components/TodoCard/TodoCard.jsx
+++ src/Components/TodoCard/TodoCard.jsx
@@ -1,26 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Checkbox from '@mui/material/Checkbox';
 import { useNavigate } from "react-router-dom";
 import './TodoCard.css';
+import axios from 'axios';
 
 const TodoCard = ({ todo }) => {
     const { _id, title, description, isCompleted } = todo;
     const [isChecked, setIsChecked] = useState(isCompleted);
     let navigate = useNavigate();
 
-    const handleCheckboxChange = (event) => {
-        setIsChecked(event.target.checked);
+    const handleCheckboxChange = (event, id) => {
+        const newIsChecked = event.target.checked
+        setIsChecked(newIsChecked);
+        axios.put(`http://localhost:3000/api/v1/todo/${id}`, {
+            isCompleted: newIsChecked
+        }).then(response => {
+            console.log(response.data);
+        }).catch(error => {
+            console.error(error);
+        })
     };
 
     return (
         <div className="container innerWidth flexColStart paddings">
             <div className='card'>
                 <div className="left flexColStart">
-                    <h4 className={`${isChecked ? 'completed' : ''} title flexEnd`} onClick={() => navigate(`/todos/${id}`)}>{title}</h4>
+                    <h4
+                        className={`${isChecked ? 'completed' : ''} title flexEnd`}
+                    // onClick={() => navigate(`/todos/${_id}`)}
+                    >{title}</h4>
                     <h6>{description}</h6>
                 </div>
                 <div className="right">
-                    <Checkbox className='flexEnd' checked={isChecked} onChange={handleCheckboxChange} />
+                    <Checkbox className='flexEnd' checked={isChecked} onChange={(event) => handleCheckboxChange(event, _id)} />
                 </div>
 
 
