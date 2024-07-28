aada9736b6d47b4c52e3e2b9b6b5b5fa334f76ce
Joseph Atef <josephatef.j97@gmail.com>
2024-07-28 22:47:07 +0300

    added Dashboard.css

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,23 +1,24 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Todolist from '../Todolist/Todolist';
 import TodoItem from '../TodoItem/TodoItem';
-import Header from '../Header/Header'
+import Header from '../Header/Header';
 import TodoForm from '../TodoForm/TodoForm';
+import './Dashboard.css';
 
 function Dashboard() {
-    return (
-        <div>
-            <Header />
-            <TodoForm />
-            <Router>
-                <Routes>
-                    <Route exact path="/" element={<Todolist />} />
-                    <Route exact path="/todos/:id" element={<TodoItem />} />
-                </Routes>
-            </Router>
-        </div>
-    )
+  return (
+    <div className="container">
+      <Header />
+      <TodoForm />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Todolist />} />
+          <Route exact path="/todos/:id" element={<TodoItem />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
